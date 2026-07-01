f877e192009de67e82e033f9b3932be9b59ebf81
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-07-01 11:55:24 -0400

    get value by name of the input field

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import SimpleForm from "./assets/Components/SimpleForm/SimpleForm";
+
+function App() {
+  return (
+    <>
+      <h1>Explore react form</h1>
+      <SimpleForm />
+      
+    </>
+  );
+}
+
+export default App;
